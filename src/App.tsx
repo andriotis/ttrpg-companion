8783771aef8d4e8fccbf413f08226e1edf7af68b
andriotis <andriotis@ece.auth.gr>
2024-12-14 19:38:14 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { motion } from "motion/react";
+import Dialog from "./components/Dialog";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full flex justify-center items-center">
+        <Dialog />
+      </div>
+    </>
+  );
+}
+
+export default App;
