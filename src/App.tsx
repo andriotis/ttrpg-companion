d6955f5abde91b2a224224b7123764f86e51d274
andriotis <andriotis@ece.auth.gr>
2024-12-15 17:50:44 +0200

    15-12-2024.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { motion } from "motion/react";
 import Dialog from "./components/Dialog";
 
 function App() {
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center">
+      <div>
         <Dialog />
       </div>
     </>
