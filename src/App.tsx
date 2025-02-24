4941443f9bcd1f2f40955ff77596803797d010be
andriotis <nikos.andriotis@outlook.com>
2025-02-24 18:33:16 +0200

    shadcn and tailwindv3 work

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { AccordionDemo } from "./AccordionDemo";
+
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>
+      <AccordionDemo />
+    </>
+  );
+}
+
+export default App;
