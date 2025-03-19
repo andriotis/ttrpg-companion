0871fb125125362015af9387367bdc324fd0d2d0
andriotis <nikos.andriotis@outlook.com>
2025-03-19 16:27:36 +0200

    add vite, tailwind, shadcn and geist

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>abcdefghijklmnopqrstuvwxyz 0123456789</Button>
+    </div>
+  );
+}
+
+export default App;
