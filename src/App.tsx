bc0dddebf45a785e53985b31b730bd7bdf89da4c
andriotis <nikos.andriotis@outlook.com>
2025-03-19 17:30:47 +0200

    add lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
 import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>abcdefghijklmnopqrstuvwxyz 0123456789</Button>
+    <div className="flex h-lvh items-center justify-center">
+      <Drawer>
+        <DrawerTrigger className="flex flex-row">
+          <Search className="inline" />
+          <span>Search</span>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
